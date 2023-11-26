6d53836eb4e2b4117f5434f635163a6ab29f4210
BKrasnov <bkrasnov235@gmail.com>
2023-11-26 23:19:05 +0700

    Update strategies design pattern

---
+++ FigureAreaCalculator/AreaCalculatorStrategy.cs
@@ -0,0 +1,30 @@
+﻿using FigureAreaCalculator.DomainServices;
+using FigureAreaCalculator.Strategies;
+using System;
+
+namespace FigureAreaCalculator
+{
+    public class AreaCalculatorStrategy<T> : IAreaCalculatorStrategy<T>
+    {
+        private readonly AreaCalculatorStrategyRegistry strategyRegistry;
+
+        public AreaCalculatorStrategy(AreaCalculatorStrategyRegistry strategyRegistry = null)
+        {
+            this.strategyRegistry = strategyRegistry ?? new AreaCalculatorStrategyRegistry();
+        }
+
+        /// <summary>
+        /// Получить площадь фигуры.
+        /// </summary>
+        public double GetArea(T figure)
+        {
+            if (figure == null)
+            {
+                throw new ArgumentNullException(nameof(figure));
+            }
+            var strategy = strategyRegistry.GetStrategy<T>();
+            return Math.Round(strategy.GetArea(figure), 2);
+        }
+
+    }
+}
