680ac37d74dd7509d2d3780a4bd76430afdf4b28
BKrasnov <bkrasnov235@gmail.com>
2023-11-26 23:41:16 +0700

    Add Client Demo project

---
+++ FigureCalculateDemoApp/ClientProgram.cs
@@ -0,0 +1,12 @@
+﻿using FigureAreaCalculator;
+using FigureAreaCalculator.DomainServices;
+using FigureCalculateDemoApp.Domains;
+using FigureCalculateDemoApp.Strategies;
+
+/// Регистрация новой стратегии, созданной клиентом.
+var registry = new AreaCalculatorStrategyRegistry();
+registry.Register(new SquareAreaCalculatorStrategy());
+
+var areaCalculator = new AreaCalculatorStrategy<Square>(registry);
+var circle = new Square(5);
+Console.WriteLine(areaCalculator.GetArea(circle));
